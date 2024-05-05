61f52ec9e5d44d7c8cabd1d40fc2276e4e3e7e87
Jose Victor <zevicctor2@gmail.com>
2024-05-04 21:54:01 -0300

    to improve setup typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
-import { User } from './users/entities/user.entity';
+import typeorm from './config/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      entities: [User],
-      synchronize: true
+    ConfigModule.forRoot({ 
+      isGlobal: true,
+      load: [typeorm]
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => (configService.get('typeorm'))
     }),
     AuthModule,
     UsersModule,
---
+++ src/config/typeorm.ts
@@ -0,0 +1,21 @@
+import { registerAs } from '@nestjs/config';
+import { config as dotenvConfig } from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+dotenvConfig({ path: '.env'});
+
+const config = {
+    type: 'postgres',
+    host: `${process.env.DATABASE_HOST}`,
+    port: `${process.env.DATABASE_PORT}`,
+    username: `${process.env.DATABASE_USERNAME}`,
+    password: `${process.env.DATABASE_PASSWORD}`,
+    database: `${process.env.DATABASE_NAME}`,
+    entities: [`${__dirname}/entities/*.entity{.ts,.js}`],
+    migrations: [`${__dirname}/migrations/*{.ts,.js}`],
+    autoLoadEntities: true,
+    synchronize: false,
+};
+
+export default registerAs('typeorm', () => config);
+export const connectionSource = new DataSource(config as DataSourceOptions);
